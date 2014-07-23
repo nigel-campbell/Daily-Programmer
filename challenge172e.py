461bd1da3e4b6810a6ddd3d2d7edd978ecee4967
nac7 <ncampbell7@gatech.edu>
2014-07-22 20:22:16 -0700

    Comments

--- challenge172e.py
+++ challenge172e.py
@@ -1,3 +1,16 @@
+# Daily Programmer Challenge #172 (www.reddit.com/r/dailyprogrammer)
+
+# A portable bitmap is one of the oldest image formats around and grants access to very simple image creation and sharing.
+# Today, you will be creating an image of this format.
+# A simple PBM program can be seen here[1] (Note that we'll be creating the simplest version, a PBM, not PPM or PGM.)
+# But basically the program consists of the following:
+# A 2byte string (usually 'P1') denoting the file format for that PBM
+# 2 integers denoting the Width and Height of our image file respectively
+# And finally, our pixel data - Whether a pixel is 1 - Black or 0 - White.
+
+# See link for details
+# http://www.reddit.com/r/dailyprogrammer/comments/2ba3g3/7212014_challenge_172_easy/
+
 def mapFileToDict():
 	letterMappings = {}
 	fileName = 'font.txt'
@@ -28,11 +41,30 @@ def getLineFromString(lineNumber, string):
 	line += ' '
 	return line
 
-def constructOutput():
+def constructOutput(mappings, inputString):
 	inputString = inputString.upper()
 	output = ''
-	
-def writeStringToPBM():
-	pass
+	for num in range(0,8):
+		for char in inputString:
+			if char == ' ':
+				continue
+			currentMap = mappings[char]
+			output += getLineFromString(num, currentMap)
+		output += '\n'
+	return output
+
+def writeStringToPBM(output, fileName):
+	f = open('Test Folder/' + fileName + '.pbm', 'w')
+	f.write(output)
+	f.close()
+
 def main():
+	mapping = mapFileToDict()
+	word = raw_input("Enter word of phrase: ")
+	output = constructOutput(mapping, word)
+	fileName = raw_input("Enter output file name: ")
+	writeStringToPBM(output, fileName)
+
+if __name__ == '__main__':
+	main()
 	pass
