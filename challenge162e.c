bb00f8790f6253bc95f88a2e28c90d807853f273
nac7 <ncampbell7@gatech.edu>
2014-07-25 18:08:29 -0700

    Got it to read dictionary

--- challenge162e.c
+++ challenge162e.c
@@ -4,19 +4,34 @@
 #define MAX_LINE 256
 #define DICTIONARY_MAX_SIZE 50
 
-typedef struct dictionary{
+typedef struct Dictionaries {
 	char* words[DICTIONARY_MAX_SIZE] ; 
-};
+} Dictionary;
 
-// Reads and prints dictionary
-void readInput(char* filePath){
+void printDictionaryContents(Dictionary *dict, int numberOfItems){
+	int i;
+	for (i = 0; i < numberOfItems; i++)
+		printf("%d: %s", i, dict->words[i]);
+}
+
+/*  Reads file contents and stores each word
+ *	into a dictionary. 
+ */
+Dictionary* readInput(char* filePath){
 	FILE *file;
 	char line[MAX_LINE];
+	Dictionary* dict = (Dictionary*) malloc(DICTIONARY_MAX_SIZE * sizeof(Dictionary));
 	file = fopen(filePath, "r");
+	int index = 0; 
 	while( fgets(line, MAX_LINE, file) ){
-		printf("%s", line);
+		char *newChar = (char*) malloc( sizeof(char) * MAX_LINE);
+		strcpy(newChar, line);
+		dict->words[index] = newChar;
+		index++;
+
 	}
 	fclose(file);
+	return dict;
 }
 
 int main(int argc, char *argv[]){
@@ -26,6 +41,7 @@ int main(int argc, char *argv[]){
 		printf("standard use: %s dictionary input\n", argv[0]);
 	}
 	else{
-		readInput(dictionary);
+		Dictionary* dict = readInput(dictionary);
+		printDictionaryContents(dict, 23);
  	}	
 }
