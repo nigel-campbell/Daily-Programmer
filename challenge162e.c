fdc12c5c4f406e47de8ac9373adb19848fc46aba
nac7 <ncampbell7@gatech.edu>
2014-07-25 19:07:02 -0700

    Added function to read raw compressed file

--- challenge162e.c
+++ challenge162e.c
@@ -2,25 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 #define MAX_LINE 256
-#define DICTIONARY_MAX_SIZE 50
+#define DICTIONARY_MAX_LENGTH 50
+#define PARAGRAPH_MAX_LENGTH 30
 
 typedef struct Dictionaries {
-	char* words[DICTIONARY_MAX_SIZE] ; 
+	char* words[DICTIONARY_MAX_LENGTH] ; 
 } Dictionary;
 
-void printDictionaryContents(Dictionary *dict, int numberOfItems){
+typedef struct Paragraphs {
+	char* lines[PARAGRAPH_MAX_LENGTH];
+} Paragraph;
+
+void printDictionaryFile(Dictionary *dict, int numberOfItems){
 	int i;
 	for (i = 0; i < numberOfItems; i++)
 		printf("%d: %s", i, dict->words[i]);
 }
+void printCompressedFile(Paragraph *content, int numberOfItems){
+	int i;
+	for (i = 0; i < numberOfItems; i++)
+		printf("%d: %s", i, content->lines[i]);
+}
 
 /*  Reads file contents and stores each word
- *	into a dictionary. 
+ *	into a paragraph. 
  */
-Dictionary* readInput(char* filePath){
+Paragraph* readCompressedFile(char* filePath){
 	FILE *file;
 	char line[MAX_LINE];
-	Dictionary* dict = (Dictionary*) malloc(DICTIONARY_MAX_SIZE * sizeof(Dictionary));
+	Paragraph* content = (Paragraph*) malloc(PARAGRAPH_MAX_LENGTH * sizeof(Paragraph));
+	file = fopen(filePath, "r");
+	int index = 0;
+	while( fgets(line, MAX_LINE, file) ){
+		char *newLine = (char*) malloc( sizeof(char) * MAX_LINE);
+		strcpy(newLine, line);
+		content->lines[index] = newLine;
+		index++;
+	}
+	fclose(file);
+	return content;
+}
+
+Dictionary* readDictionary(char* filePath){
+	FILE *file;
+	char line[MAX_LINE];
+	Dictionary* dict = (Dictionary*) malloc(DICTIONARY_MAX_LENGTH * sizeof(Dictionary));
 	file = fopen(filePath, "r");
 	int index = 0; 
 	while( fgets(line, MAX_LINE, file) ){
@@ -28,7 +54,6 @@ Dictionary* readInput(char* filePath){
 		strcpy(newChar, line);
 		dict->words[index] = newChar;
 		index++;
-
 	}
 	fclose(file);
 	return dict;
@@ -36,12 +61,15 @@ Dictionary* readInput(char* filePath){
 
 int main(int argc, char *argv[]){
 	char *dictionary = argv[1];
-	char *text = argv[2];
+	char *compressedFile = argv[2];
 	if( argc != 3 ){
 		printf("standard use: %s dictionary input\n", argv[0]);
 	}
 	else{
-		Dictionary* dict = readInput(dictionary);
-		printDictionaryContents(dict, 23);
+		Dictionary* dict = readDictionary(dictionary);
+		Paragraph* text = readCompressedFile(compressedFile);
+		printCompressedFile(text, 6);
+		printDictionaryFile(dict, 23);
  	}	
+
 }
